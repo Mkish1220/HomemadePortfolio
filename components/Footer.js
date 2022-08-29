41c2dd6b21413aea9cf0aa538f2090e417932005
MKish420 <meaghanmpeters@hotmail.com>
2022-08-29 09:31:44 -0400

    added nav bar header and footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import icons for github 
+// import icons for linkedin
+import "../styles/Footer.css";
+
+function Footer()  {
+    return (
+        <div className="footer">
+            <div className="socialConnection">
+                {/* <GithubIcon /> */}
+                {/* <LinkedinIcon /> */}
+            </div>
+            <p>This Portfolio was generated with ❤️ by &copy;Mkish1220 with the help of UCF Full Stack Web Devlopment Bootcamp</p>
+        </div>
+    );
+}
+
+export default Footer;
