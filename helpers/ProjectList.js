5ad76a901732c497654d15e1f00c074e125e9125
MKish420 <meaghanmpeters@hotmail.com>
2022-08-29 09:30:27 -0400

    added helpers project list

---
+++ helpers/ProjectList.js
@@ -0,0 +1,19 @@
+import Proj1 from "../assests/proj1.jpg";
+import Proj2 from "../assests/proj2.png";
+// import Proj3 from "../assests/proj1.jpg";
+// import Proj4 from "../assests/proj1.jpg";
+
+export const ProjectList = [
+    {
+        name:"Travel Agents: A Guide to Traveling",
+        image: Proj1,
+        skills: "Javascript, HTML, CSS",
+    },
+    {
+        name: "Carbon Combaters: Changing Our Carbon Footprint one print at a time ",
+        image: Proj2,
+        // add other tech to skills from carbon repo
+        skills: "Javascript, HTML, CSS "
+    },
+];
+// add next project and other personal projects 
